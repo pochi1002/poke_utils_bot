016a59082b5d6716c7214a948908029735784b20
takuya kikuchi <t.kikuchi.pch@gmail.com>
2017-01-31 00:54:34 +0900

    VCメタモンの経験値から性格を求める処理を試しに追加。

--- PokeUtils/Controllers/LineMessagesController.cs
+++ PokeUtils/Controllers/LineMessagesController.cs
@@ -34,7 +34,10 @@ namespace PokeUtils.Controllers
                 LineMessageHandler handler = new LineMessageHandler(lineEvent);
 
                 Profile profile = await handler.GetProfile(lineEvent.Source.UserId);
-
+                //if(profile == null)
+                //{
+                //    return Request.CreateResponse(HttpStatusCode.OK);
+                //}
                 switch (lineEvent.Type)
                 {
                     case EventType.Beacon:
@@ -169,10 +172,20 @@ namespace PokeUtils.Controllers
             }
             else
             {
-                replyMessage = new TextMessage(textMessage.Text);
+                var intValue = 0;
+                if (int.TryParse(textMessage.Text, out intValue))
+                {
+                    var personarity = (Personarities)(intValue % 25);
+                    replyMessage = new TextMessage($"{personarity.PersonarityToString()} なメタモンだね。");
+                } else
+                {
+                    replyMessage = new TextMessage(null);
+                }
             }
             await Reply(replyMessage);
         }
+
+
 
         public async Task HandleMediaMessage()
         {
@@ -222,6 +235,106 @@ namespace PokeUtils.Controllers
             catch
             {
                 await lineClient.PushAsync(lineEvent.CreatePush(message: replyMessage));
+            }
+        }
+    }
+
+    public enum Personarities
+    {
+        Ganbaruya = 0,
+        Samisigari,
+        Yuukan,
+        Ijippari,
+        Yancha,
+        Zubutoi,
+        Sunao,
+        Nonki,
+        Wanpaku,
+        Noutenki,
+        Okubyou,
+        Sekkachi,
+        Majime,
+        Youki,
+        Mujaki,
+        Hikaeme,
+        Ottori,
+        Reisei,
+        Tereya,
+        Ukkariya,
+        Odayaka,
+        Otonashii,
+        Namaiki,
+        Shincho,
+        Kimagure = 24,
+        Unknown,
+    }
+
+    public static class PersonalityUtils
+    {
+        public static Personarities GetPersonarityFromString(string str)
+        {
+            switch (str)
+            {
+                case "がんばりや": return Personarities.Ganbaruya;
+                case "さみしがり": return Personarities.Samisigari;
+                case "ゆうかん": return Personarities.Yuukan;
+                case "いじっぱり": return Personarities.Ijippari;
+                case "やんちゃ": return Personarities.Yancha;
+                case "ずぶとい": return Personarities.Zubutoi;
+                case "すなお": return Personarities.Sunao;
+                case "のんき": return Personarities.Nonki;
+                case "わんぱく": return Personarities.Wanpaku;
+                case "のうてんき": return Personarities.Noutenki;
+                case "おくびょう": return Personarities.Okubyou;
+                case "せっかち": return Personarities.Sekkachi;
+                case "まじめ": return Personarities.Majime;
+                case "ようき": return Personarities.Youki;
+                case "むじゃき": return Personarities.Mujaki;
+                case "ひかえめ": return Personarities.Hikaeme;
+                case "おっとり": return Personarities.Ottori;
+                case "れいせい": return Personarities.Reisei;
+                case "てれや": return Personarities.Tereya;
+                case "うっかりや": return Personarities.Ukkariya;
+                case "おだやか": return Personarities.Odayaka;
+                case "おとなしい": return Personarities.Otonashii;
+                case "なまいき": return Personarities.Namaiki;
+                case "しんちょう": return Personarities.Shincho;
+                case "きまぐれ": return Personarities.Kimagure;
+                default:
+                    return Personarities.Unknown;
+            }
+        }
+        public static string PersonarityToString(this Personarities self)
+        {
+            switch (self)
+            {
+                case Personarities.Ganbaruya: return "がんばりや";
+                case Personarities.Hikaeme: return "ひかえめ";
+                case Personarities.Ijippari: return "いじっぱり";
+                case Personarities.Kimagure: return "きまぐれ";
+                case Personarities.Majime: return "まじめ";
+                case Personarities.Mujaki: return "むじゃき";
+                case Personarities.Namaiki: return "なまいき";
+                case Personarities.Nonki: return "のんき";
+                case Personarities.Noutenki: return "のうてんき";
+                case Personarities.Odayaka: return "おだやか";
+                case Personarities.Okubyou: return "おくびょう";
+                case Personarities.Otonashii: return "おとなしい";
+                case Personarities.Ottori: return "おっとり";
+                case Personarities.Reisei: return "れいせい";
+                case Personarities.Samisigari: return "さみしがり";
+                case Personarities.Sekkachi: return "せっかち";
+                case Personarities.Shincho: return "しんちょう";
+                case Personarities.Sunao: return "すなお";
+                case Personarities.Tereya: return "てれや";
+                case Personarities.Ukkariya: return "うっかりや";
+                case Personarities.Wanpaku: return "わんぱく";
+                case Personarities.Yancha: return "やんちゃ";
+                case Personarities.Youki: return "ようき";
+                case Personarities.Yuukan: return "ゆうかん";
+                case Personarities.Zubutoi: return "ずぶとい";
+                case Personarities.Unknown:
+                default: return "ふめい";
             }
         }
     }
